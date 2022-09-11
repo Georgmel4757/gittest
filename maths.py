ff1a7c1223f859dbc8674a32de81d4a0de3a76f1
George <georgmel4757@gmail.com>
2022-09-11 20:06:19 +0300

    Add class Vector2 in maths.py

--- maths.py
+++ maths.py
@@ -1,4 +1,19 @@
-__all__ = ['add', 'sub', 'mul', 'div', 'fibonacci']
+__all__ = ['Vector2', 'add', 'sub', 'mul', 'div', 'fibonacci']
+
+
+class Vector2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2(self.x - other.x, self.y - other.y)
+
+    def __abs__(self):
+        return (self.x ** 2 + self.y ** 2) ** 0.5
 
 
 def add(a, b):
